74dbe767ea0980e973246088cce14e49192e7015
amos <amoshochman1982@gmail.com>
2020-01-22 20:02:17 +0200

    removing foreign keys and minor changes

--- backend/config.py
+++ backend/config.py
@@ -9,33 +9,15 @@ tables = {
                 "(id INT AUTO_INCREMENT PRIMARY KEY,"
                 "name VARCHAR(255))",
 
-    # "seniors_missions": "CREATE TABLE seniors_missions"
-    #                     "(id INT AUTO_INCREMENT PRIMARY KEY,"
-    #                     "senior_id INT,"
-    #                     "mission_id INT,"
-    #                     "FOREIGN KEY(senior_id) REFERENCES seniors(id),"
-    #                     "FOREIGN KEY(mission_id) REFERENCES missions(id))",
-
     "requests": "CREATE TABLE requests"
                 "(id INT AUTO_INCREMENT PRIMARY KEY,"
                 "senior_id INT,"
-                "mission_id INT,"
+                "mission_name VARCHAR(255),"
                 "status INT,"
                 "request_time TIME,"
                 "mission_start TIME,"
                 "mission_end TIME,"
                 "longitude DOUBLE,"
-                "latitude DOUBLE,"
-                "FOREIGN KEY(senior_id) REFERENCES seniors(id),"
-                "FOREIGN KEY(mission_id) REFERENCES missions(id))",
-
-    # "physical_issues": "CREATE TABLE physical_issues"
-    #                    "(id INT AUTO_INCREMENT PRIMARY KEY,"
-    #                    "name VARCHAR(255))",
-
-    # "physical_issues_seniors": "CREATE TABLE physical_issues_seniors"
-    #                            "(id INT AUTO_INCREMENT PRIMARY KEY,"
-    #                            "physical_issue_id INT,"
-    #                            "senior_id INT)",
+                "latitude DOUBLE)"
 
 }

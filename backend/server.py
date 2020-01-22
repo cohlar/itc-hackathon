fd6bdf6f90db29c3b28a1a17320dea189ed5dd0e
cohlar <cohlar@gmail.com>
2020-01-22 12:08:34 +0200

    first commit

---
+++ backend/server.py
@@ -0,0 +1,25 @@
+import logging
+from flask import Flask, jsonify
+from flask_cors import CORS
+from utils.utils import filter_server_log_messages
+
+
+def create_app():
+    app = Flask(__name__)
+    CORS(app)
+    return app
+
+
+app = create_app()
+filter_server_log_messages()
+
+
+@app.route('/api/users/<int:id>', methods=['GET'])
+def get_user(id):
+    user = {}
+    #TODO: get user from DB
+    return jsonify(user)
+
+
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)

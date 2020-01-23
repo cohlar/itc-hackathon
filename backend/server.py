64b8c49b0519db54d75e046e7927a60865a85640
jvinetz <julianvinetz@gmail.com>
2020-01-23 10:37:45 +0200

    get_requests add names

--- backend/server.py
+++ backend/server.py
@@ -1,11 +1,10 @@
-import logging
-import mysql.connector
-from flask import Flask, jsonify
-from flask_cors import CORS
-from utils.server import filter_server_log_messages
-from utils.mysql_con import sql
 import json
-from utils.MYDBUtils import DBConnector, DBCreator
+
+from flask import Flask
+from flask_cors import CORS
+from utils.MYDBUtils import DBConnector
+from utils.mysql_con import sql
+from utils.server import filter_server_log_messages
 
 
 def create_app():
@@ -22,14 +21,15 @@ filter_server_log_messages()
 def get_requests(lat, lon, up_to):
     """Get requests near volunteer"""
     my_connector = DBConnector(sql["host"], sql["user"], sql["passwd"], sql["db"])
-    keys = ['tel', 'age', 'mis_id', 'lat', 'lon']
+    keys = ['name', 'tel', 'age', 'mis_id', 'lat', 'lon']
 
     # distance is in m
     distance = f'(6371000 * acos(cos(radians({lat})) * cos(radians(latitude)) * cos(radians(longitude) - radians({lon}))' \
                f' + sin(radians({lat}))* sin(radians(latitude))))'
-    data_list = my_connector.select("seniors.tel, YEAR(CURDATE()) - seniors.birth_year, requests.mission_id, "
-                                    "requests.latitude, requests.longitude", "requests, seniors",
-                                    f'{distance} < {up_to} and requests.senior_id = seniors.id')
+    data_list = my_connector.select(
+        "seniors.name, seniors.tel, YEAR(CURDATE()) - seniors.birth_year, requests.mission_id, "
+        "requests.latitude, requests.longitude", "requests, seniors",
+        f'{distance} < {up_to} and requests.senior_id = seniors.id')
     data_dicts = [dict(zip(keys, item)) for item in data_list]
     return json.dumps(data_dicts)
 
